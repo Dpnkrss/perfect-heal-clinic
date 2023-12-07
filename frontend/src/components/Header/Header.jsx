83c64bca46c45b7d5d0222a594129074f89bf87c
Deepankar <deepankarsalhan@gmail.com>
2023-12-07 13:33:45 -0700

    added logo

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,5 +1,42 @@
+import { useEffect, useRef } from 'react';
+import React from 'react';
+import logo from '../../assets/logo/Perfect-Heal-Ortho-General-Clinic_Logo-01-e1672666778585-300x99.png';
+import { NavLink, Link } from 'react-router-dom';
+
+const navLinks = [
+  {
+    path: '/home',
+    display: 'About us',
+  },
+  {
+    path: '/treatment',
+    display: 'Treatments',
+  },
+  {
+    path: '/facilities',
+    display: 'Facilities',
+  },
+  {
+    path: '/contactus',
+    display: 'Contact us',
+  },
+];
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className='header flex items-center'>
+      <div className='container'>
+        <div className='flex items-center justify-between'>
+          <div>
+            <img src={logo} alt='Perfect heal' />
+          </div>
+
+          <div className='navigation'>
+            <ul className='menu flex items-center gap-[2.7rem]'></ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
