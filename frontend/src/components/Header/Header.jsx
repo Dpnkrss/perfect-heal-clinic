a3d6949dd3b10267aa491581dc54497a47553def
Deepankar <deepankarsalhan@gmail.com>
2023-12-07 15:24:44 -0700

    nav bar css added

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef } from 'react';
 import React from 'react';
 import logo from '../../assets/logo/Perfect-Heal-Ortho-General-Clinic_Logo-01-e1672666778585-300x99.png';
+import phoneimg from '../../assets/phone_calling.gif';
 import { NavLink, Link } from 'react-router-dom';
 
 const navLinks = [
@@ -23,13 +24,14 @@ const navLinks = [
 ];
 const Header = () => {
   return (
-    <header className='header flex items-center'>
+    <header className='header flex items-center px-20'>
       <div className='container'>
-        <div className='flex items-center justify-between'>
+        <div className='flex items-center justify-between m-4 '>
+          {/*.....logo........*/}
           <div>
             <img src={logo} alt='Perfect heal' />
           </div>
-
+          {/*.....Navigabar........*/}
           <div className='navigation'>
             <ul className='menu flex items-center gap-[2.7rem]'>
               {navLinks.map((link, index) => (
@@ -47,6 +49,24 @@ const Header = () => {
                 </li>
               ))}
             </ul>
+          </div>
+
+          {/*....nav right......*/}
+          <div className='inline-flex'>
+            <Link to='/'>
+              <figure className='w-[50px] h-[50px] rounded-full'>
+                <img
+                  src={phoneimg}
+                  className='rounded-full animate-bounce h-[50px] w-[50px] '
+                  alt='Phone number'
+                />
+              </figure>
+            </Link>
+            <Link to='/'>
+              <span className='text-[#CF2234] text-xl font-bold mt-2'>
+                80541 02361
+              </span>
+            </Link>
           </div>
         </div>
       </div>
