4a281c84da184b5c321af10b9ea487ca3fa89fec
Vivek <veeveckjeet@gmail.com>
2023-12-13 09:51:37 -0500

    Card Component n related functionalities

---
+++ frontend/src/pages/SportInjuries.jsx
@@ -0,0 +1,37 @@
+const SportInjuries = () => {
+  return (
+    <>
+      <p>
+        Orthopaedics is a medical speciality that focuses on the diagnosis,
+        treatment and prevention of disorders of the musculoskeletal system. It
+        includes bones, joints, ligaments, tendons, muscles and nerves.
+        Orthopaedic sugeon at Perfect Heal Ortho and General Clinic uses a
+        variety of methods to diagnose and treat musculoskeletal conditions,
+        including physical examinations, imaging tests (such as x-rays and MRIs)
+        and surgical procedures. They also recommend non-surgical treatment
+        options such as physical therapy, medication and lifestyle changes to
+        help patients manage their conditions and improve their mobility and
+        function, depending on their specific needs and conditions. Our sports
+        injuries department specialises in the diagnosis, treatment and
+        management of injuries sustained during sports or physical activity.
+        These injuries can range from acute injuries such as fractures or
+        ligament tears, to overuse injuries, which occur from repetitive stress
+        on a particular part of the body. Some common sports injuries include
+        sprains, strains and overuse injuries of the knee, shoulder and ankle.
+        ARTHOSCOPY: A minimally invasive surgical procedure that allows doctors
+        to see inside a joint and make repairs without making a large incision.
+        ARTHROPLASTY: Aiding in restoring function and improving quality of life
+        for people with severe joint damage or chronic pain in the hip. BONE
+        FRACTURES: A break in the bone doesn’t have to break your spirit. From
+        diagnosis to recovery, we’ve got you covered for all your bone fracture
+        needs. SPORTS TEARS (MENISCAL TEARS/ LIGAMENT TEARS): Sports injuries
+        don’t have to slow you down. Find relief and healing with our
+        specialised care. RHEUMATOID ARTHRITIS: Rheumatoid arthritis can be
+        challenging, but with the right care and support, it’s possible to lead
+        a fulfilling and active life.
+      </p>
+    </>
+  );
+};
+
+export default SportInjuries;
