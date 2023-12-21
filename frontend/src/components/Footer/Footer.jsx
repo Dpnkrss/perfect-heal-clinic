27486563941eb6eb241c048a64fd435deb4b5028
Deepankar <deepankarsalhan@gmail.com>
2023-12-20 20:09:59 -0700

    footer finalised with alignments

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import logo from '../../assets/footer/Perfect-Heal-Ortho-General-Clinic_Logo-02-e1672842553297.png';
+import mailIcon from '../../assets/footer/mail.svg';
+import locationIcon from '../../assets/footer/location_on.svg';
+import callIcon from '../../assets/footer/phone.svg';
 
 const Footer = () => {
   return (
-    <footer className='bg-[#CF2234] text-white text-center p-4 h-[600px] '>
+    <footer className='bg-[#CF2234] text-white text-center p-4 h-[650px] '>
       <div className='max-w-screen-xl mx-auto px-4 md:mt-8'>
         <img src={logo} alt='Perfect Heal Logo' className='mx-auto h-[100px]' />
 
@@ -31,31 +34,15 @@ const Footer = () => {
         {/*cards*/}
 
         <div className='grid grid-cols-1 md:grid-cols-3 divide-x divide-black md:divide-x-0 md:mt-10 rounded-lg overflow-hidden shadow-md'>
-          <div className='bg-white p-4 shadow-md'>
-            <svg
-              class='h-10 w-10 text-red-500'
-              width='24'
-              height='24'
-              viewBox='0 0 24 24'
-              stroke-width='2'
-              stroke='currentColor'
-              fill='none'
-              stroke-linecap='round'
-              stroke-linejoin='round'
-            >
-              {' '}
-              <path stroke='none' d='M0 0h24v24H0z' />{' '}
-              <polyline points='3 9 12 15 21 9 12 3 3 9' />{' '}
-              <path d='M21 9v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10' />{' '}
-              <line x1='3' y1='19' x2='9' y2='13' />{' '}
-              <line x1='15' y1='13' x2='21' y2='19' />
-            </svg>
+          <div className='bg-white p-14 shadow-md flex flex-col items-center'>
+            <img src={mailIcon} className='w-12 h-12' alt='Mail Icon' />
             <h4 className='font-bold text-red-600'>Mail</h4>
             <p className='mt-2 text-black font-bold'>
               info@perfecthealclinic.com
             </p>
           </div>
-          <div className='bg-white p-4  shadow-md'>
+          <div className='bg-white p-10  shadow-md flex flex-col items-center'>
+            <img src={locationIcon} className='w-12 h-12' alt='Mail Icon' />
             <h4 className='font-bold text-red-600'>Address</h4>
             <address className='not-italic mt-2 font-bold text-black'>
               Perfect Heal
@@ -67,13 +54,14 @@ const Footer = () => {
               Jalandhar - 144 003
             </address>
           </div>
-          <div className='bg-white p-4 shadow-md'>
+          <div className='bg-white p-14 shadow-md flex flex-col items-center'>
+            <img src={callIcon} className='w-12 h-12' alt='Mail Icon' />
             <h4 className='font-bold text-red-600'>Call</h4>
             <p className='mt-2 text-black font-bold'>8054102361</p>
           </div>
         </div>
 
-        <div className='border-t border-white mt-8 pt-4'>
+        <div className='border-t border-dotted border-white opacity-75 mt-8 pt-4 flex justify-between items-center'>
           <p className='text-sm'>
             Copyright © 2023 Perfect Heal Ortho and General Clinic
           </p>
