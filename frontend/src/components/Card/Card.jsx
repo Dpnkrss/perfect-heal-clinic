ef38b049e96a1cf3de15849aae43dc9e0134c9ad
Deepankar <deepankarsalhan@gmail.com>
2023-12-21 01:37:59 -0700

    put trasnparent card and colour fix on Speciaties and card component

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,27 +1,49 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 // eslint-disable-next-line react/prop-types
 const Card = ({ title, content, linkTo, topImg, bottomImg }) => {
+  const bgColorClass =
+    title === 'Internal Medicine' ? 'bg-[#F15A2D]' : 'bg-slate-300';
+  const isInternalMedicine = title === 'Internal Medicine';
+  const textColorClass = title ? 'text-white' : 'text-black-300';
+  const titleColorClass = isInternalMedicine ? 'text-white' : 'text-black';
+  const linkTextColorClass = title ? 'text-white' : 'text-orange-500';
+  const linkBgColorClass = title ? 'bg-[#F15A2D]' : 'bg-slate-300';
+  console.log(title);
   return (
-    <div className="relative bg-slate-300 roundex x1 max-w-md  max-h-[19rem] p-8">
+    <div
+      className={`relative ${bgColorClass} roundex x1 max-w-md  max-h-[19rem] p-8 rounded-lg`}
+    >
       {topImg && (
-        <div className="top-0 left-0 w-12 h-12 m-5 block">
+        <div className='top-0 left-0 w-12 h-12 m-5 block'>
           <img src={topImg} />
         </div>
       )}
-      <div className="text-left mb-5">
-        <h2 className="text-xl font-semibold text-orange-400">{title}</h2>
+      <div className='text-left mb-5'>
+        <h2
+          className={`text-xl font-semibold ${
+            isInternalMedicine ? 'text-white' : 'text-black'
+          }`}
+        >
+          {title}
+        </h2>
       </div>
-      <div className="text-orange-600 mb-5">{content}</div>
+      <div
+        className={`mb-5 ${textColorClass} ${
+          isInternalMedicine ? 'text-white' : 'text-black'
+        } `}
+      >
+        {content}
+      </div>
       {bottomImg && (
-        <div className="absolute bottom-0 right-0 w-12 h-12 m-5">
+        <div className='absolute bottom-0 right-0 w-12 h-12 m-5'>
           <img src={bottomImg} />
         </div>
       )}
-      <div className="mb-4">
+      <div className='mb-4'>
         <Link
           to={linkTo}
-          className="text-orange-500 inline-block px-4 py-2 bg-slate-300 rounded transition duration-300 hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
+          className={`inline-block px-4 py-2 ${linkBgColorClass} ${linkTextColorClass} rounded transition duration-300 hover:bg-blue-500 focus:outline-none focus:bg-blue-500`}
         >
           Read More
         </Link>
