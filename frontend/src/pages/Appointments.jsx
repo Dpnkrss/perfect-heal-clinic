b7397d85a1e005ce0b7dec51d881d549b7ac2cb4
Deepankar <deepankarsalhan@gmail.com>
2024-01-11 18:17:19 -0700

    pagination added using react-paginate

--- frontend/src/pages/Appointments.jsx
+++ frontend/src/pages/Appointments.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Sidebar from '../components/Sidebar/Sidebar';
+import ReactPaginate from 'react-paginate';
 
 export const Appointments = () => {
   const [appointments, setAppointments] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const appointmentsPerPage = 8;
 
   const getAppointments = async () => {
     try {
@@ -39,6 +42,9 @@ export const Appointments = () => {
     getAppointments();
   }, []);
 
+  //calculates the total number of pages needed for pagination based on the number of appointments and how many appointments are to displayed per page.ceil returns whole number
+  const pageCount = Math.ceil(appointments.length / appointmentsPerPage);
+
   const getSpecialityText = (specialityValue) => {
     const value = parseInt(specialityValue, 10);
     if (value >= 37 && value <= 41) {
@@ -49,6 +55,16 @@ export const Appointments = () => {
       return 'Unknown Speciality'; // Default text if the value doesn't match any range
     }
   };
+
+  const handlePageClick = (event) => {
+    setCurrentPage(event.selected);
+  };
+
+  // it will give first 8 appointments to show in current index page
+  const displayedAppointments = appointments.slice(
+    currentPage * appointmentsPerPage,
+    (currentPage + 1) * appointmentsPerPage
+  );
 
   return (
     <div className='flex'>
@@ -85,7 +101,7 @@ export const Appointments = () => {
               </tr>
             </thead>
             <tbody>
-              {appointments.map((appointment) => (
+              {displayedAppointments.map((appointment) => (
                 <tr
                   key={appointment._id}
                   className='bg-white border-b dark:bg-gray-800 dark:border-gray-700'
@@ -97,12 +113,37 @@ export const Appointments = () => {
                   <td className='py-4 px-6'>
                     {getSpecialityText(appointment.specialities)}
                   </td>
-                  <td className='py-4 px-6'>{appointment.appointmentDate}</td>
-                  <td className='py-4 px-6'>{appointment.appointmentTime}</td>
+                  <td className='py-4 px-6 whitespace-nowrap'>
+                    {appointment.appointmentDate}
+                  </td>
+                  <td className='py-4 px-6 whitespace-nowrap'>
+                    {appointment.appointmentTime}
+                  </td>
                 </tr>
               ))}
             </tbody>
           </table>
+          <ReactPaginate
+            previousLabel={'Previous'}
+            nextLabel={'Next'}
+            breakLabel={'...'}
+            pageCount={pageCount}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={handlePageClick}
+            containerClassName={'flex items-center justify-center my-4'}
+            pageLinkClassName={
+              'mx-2 px-3 py-1 border rounded hover:bg-red-500 hover:text-white'
+            }
+            previousLinkClassName={
+              'mx-2 px-3 py-1 border rounded hover:bg-red-500 hover:text-white'
+            }
+            nextLinkClassName={
+              'mx-2 px-3 py-1 border rounded hover:bg-red-500 hover:text-white'
+            }
+            breakLinkClassName={'mx-2 px-3 py-1 border rounded'}
+            activeLinkClassName={'bg-red-500 text-white rounded-full'}
+          />
         </div>
       </div>
     </div>
